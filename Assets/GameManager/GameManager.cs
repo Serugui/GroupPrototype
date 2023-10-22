0cbef636c4ed882d24cc8d25f63c3ee6da2f7b51
Sergi Ramon <bi23tl@student.sunderland.ac.uk>
2023-10-22 22:32:19 +0100

    Player input almost complete
    I´ve added the player input controls with the acc, brake and turn actions. Left to do add player and work in driving controller to test if it works

---
+++ Assets/GameManager/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // DESIGN PATTERN: SINGLETON
+    public static GameManager Instance { get; private set; }
+    public static InputManager InputManager { get; private set; }
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+
+        InputManager = new InputManager();
+    }
+}
