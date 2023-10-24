19a9722813b52b33e0462c4129a8c29de26fe7b5
Sergi Ramon <bi23tl@student.sunderland.ac.uk>
2023-10-24 11:08:43 +0100

    Joshs track and controls working

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -17,6 +18,6 @@ public class GameManager : MonoBehaviour
         }
         Instance = this;
 
-        InputManager = new InputManager();
+        InputManager = this.AddComponent<InputManager>();
     }
 }
