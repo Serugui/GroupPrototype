f4daaed14db222460ea35b429ad15f68346da004
Crystal-Pro-2024 <123314475+Crystal-Pro-2024@users.noreply.github.com>
2023-10-26 10:45:55 +0100

    menu works now

--- Assets/Menu/Test.cs
+++ Assets/Menu/Test.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class Test : MonoBehaviour
+
+
+public class ClickExample : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button yourButton;
+
     void Start()
     {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+            
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void TaskOnClick()
     {
-        
+        Debug.Log("You have clicked the button!");
+        SceneManager.LoadScene(1);
     }
 }
+
+
+
+
+        
+        
